f5734cf5d075842ae3efd9950b337aa0970c47fe
tuuyennee <86772827+tuuyennee@users.noreply.github.com>
2021-07-01 17:10:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -7,12 +7,12 @@ const CONFIG = {
     title: 'Nếu anh có tình cảm với em...',
     desc: 'Em mong anh hãy cho em câu trả lời...  ',
     btnYes: 'Anh thích em <33',
-    btnNo: 'Không, Cô Bé à :3',
+    btnNo: 'Không, Cô Bé à :33',
     question:'Cuộc sống đã cho chúng ta gặp được nhau... em rất biết ơn điều đó <3 Cho em biết cảm nhận của anh về em nhé :3',
     btnReply: 'Gửi cho em <3',
     reply: 'Một cô bé nhạy cảm với tình yêu, suy nghĩ nhiều, đã đến với cuộc đời anh quá sớm khi mà anh chưa kịp chuẩn bị mọi thứ để có thể đáp lại tình cảm ấy... đúng hong nhỉ <33333333',
     mess: 'Cảm ơn anh, nếu có thể, em cũng ước rằng mình được làm gì đó của anh 😘😘',
     messDesc: 'Xem được rồi thì nhắn tin cho em nhé <3.',
     btnAccept: 'Okiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/lychee0102
 }
