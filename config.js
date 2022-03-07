3ba7518a799dac286e0257b3b0898d6665e52fd9
tuuyennee <86772827+tuuyennee@users.noreply.github.com>
2022-03-07 15:16:24 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,21 +1,23 @@
 const CONFIG = {
     introTitle: 'Anh à!',
-    introDesc: `Trái đất vốn lạ thường
+    introDesc: `Trái đất vốn lạ thường,
     
-    Mà sao em cứ đi nhầm đường
+    Mà sao em cứ đi nhầm đường,
     
-    Lạc vào tim anh lẻ loi
+    Lạc vào tim anh lẻ loi,
     
-    Đằng sau chữ yêu đây là thương <3`,
+    Đằng sau chữ yêu đây là thương ❤️`,
     btnIntro: 'Hahahaa <33',
-    title: 'Có phải anh đang cảm thấy mệt mỏi vì công việc hong?',
+    title: 'Có phải anh đang cảm thấy mệt mỏi vì công việc hong? 😢',
     desc: 'Không sao mà em đây rồi, em sẽ giúp anh vượt qua sự áp lực đó nhé!! Dù chỉ một phần nhỏ <333 ',
     btnYes: 'Đúng rồi <33',
-    btnNo: 'Không, Cô Bé à :3 ',
-    question:'Trái đất có 7 tỉ người, tại sao anh lại chọn em để ib thế???? :3',
+    btnNo: 'Không, Cô Bé à :(( ',
+    question:'Trái đất có 7 tỉ người, được nói chuyện với anh là một cơ duyên rồi :3',
+    'mai sau em không biết thế nào nhưng... khoảng thời gian này'
+    'dù chỉ 1% cơ hội em cũng sẽ cố gắng để lại những kí ức đẹp về anh :33'
+    'Em cũng tò mò tại sao anh lại ib em giữa 2k học sinh Trần Phú 😆??'
     btnReply: 'Gửi cho em <3',
-    reply: 'Vì em xinh đẹp, em cute phô mai que, em đáng yêu (phải hơm)',
+    reply: 'Vì em xinh đẹp, em cute phô mai que, em đáng yêu (phải hơm) hahaha',
     mess: 'Here on earth, joy is yours <3333',
-    messDesc: 'Vì một tương lai ngày nào cũng là chủ nhật =))) FIGHTINGGG <333',
-    btnAccept: 'Okiiii lun <3',
-    messLink: 'http://www.facebook.com/lychee0102/' }
+    messDesc: 'Vì một tương lai mọi ngày đều là chủ nhật =))) FIGHTINGGG <333',
+    btnAccept: 'Okiiii lun <3'
