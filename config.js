ce1321bb17c98922f606e3df6e5a27c16e42a9ba
tuuyennee <86772827+tuuyennee@users.noreply.github.com>
2022-03-07 15:40:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,18 @@
 const CONFIG = {
-    introTitle: 'Anh à!',
-    introDesc: 'Trái đất vốn lạ thường,
+    introTitle: 'Babe à!',
+    introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương ❤️',
-    btnIntro: 'Hahahaa <33',
-    title: 'Có phải anh đang cảm thấy mệt mỏi vì công việc hong? 😢',
-    desc: 'Không sao mà em đây rồi, em sẽ giúp anh vượt qua sự áp lực đó nhé!! Dù chỉ một phần nhỏ <333 ',
-    btnYes: 'Đúng rồi <33',
-    btnNo: 'Không, Cô Bé à :(( ',
-    question:'Trái đất có 7 tỉ người, được nói chuyện với anh là một cơ duyên rồi :3',
-    'mai sau em không biết thế nào nhưng... khoảng thời gian này'
-    'dù chỉ 1% cơ hội em cũng sẽ cố gắng để lại những kí ức đẹp về anh :33'
-    'Em cũng tò mò tại sao anh lại ib em giữa 2k học sinh Trần Phú 😆??'
-    btnReply: 'Gửi cho em <3',
-    reply: 'Vì em xinh đẹp, em cute phô mai que, em đáng yêu (phải hơm) hahaha',
-    mess: 'Here on earth, joy is yours <3333',
-    messDesc: 'Vì một tương lai mọi ngày đều là chủ nhật =))) FIGHTINGGG <333',
-    btnAccept: 'Okiiii lun <3'
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'hihi',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích anh <33',
+    btnNo: 'Không, Anh trai à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'http://fb.com' //https://www.facebook.com/lychee0102/}
